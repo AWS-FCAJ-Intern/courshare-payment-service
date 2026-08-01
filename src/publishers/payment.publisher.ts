52389f46c206b4664dd6a9814c987c12c08a09fc
Phan Quoc Huu <phanquochuuwx@gmail.com>
2026-08-01 19:14:01 +0700

    implement sqs queue for this service

--- src/publishers/payment.publisher.ts
+++ src/publishers/payment.publisher.ts
@@ -1,25 +1,21 @@
-import {
-    SendMessageCommand
-} from "@aws-sdk/client-sqs";
-
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { sqsClient } from "../config/sqs";
-
 import { PaymentEvent } from "../events/payment.event";
 
 export class PaymentPublisher {
+  private readonly queueUrl = (() => {
+    const url = process.env.PAYMENT_EVENTS_QUEUE_URL;
+    if (!url) throw new Error('PAYMENT_EVENTS_QUEUE_URL is not defined');
+    return url;
+  })();
 
-    async publish(event: PaymentEvent): Promise<void> {
-
-        const command = new SendMessageCommand({
-
-            QueueUrl: process.env.PAYMENT_EVENTS_QUEUE_URL,
-
-            MessageBody: JSON.stringify(event)
-
-        });
-
-        await sqsClient.send(command);
-    }
-
+  async publish(event: PaymentEvent): Promise<void> {
+    const command = new SendMessageCommand({
+      QueueUrl: this.queueUrl,
+      MessageBody: JSON.stringify(event),
+    });
+    await sqsClient.send(command);
+  }
 }
+
 export const paymentPublisher = new PaymentPublisher();
