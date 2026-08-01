ffa04326be81975e9c0040c73036574989f48567
Phan Quoc Huu <phanquochuuwx@gmail.com>
2026-08-01 19:29:20 +0700

    update sqs.ts

--- src/config/sqs.ts
+++ src/config/sqs.ts
@@ -1,9 +1,5 @@
 import { SQSClient } from "@aws-sdk/client-sqs";
 
 export const sqsClient = new SQSClient({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
-        secretAccessKey: process.env.AWS_SECRET_KEY!
-    }
+    region: process.env.AWS_REGION || "ap-southeast-1",
 });
